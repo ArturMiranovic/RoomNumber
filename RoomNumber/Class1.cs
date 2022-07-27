16ab24d544d919c0ea875f35e23e5b24883b0e71
Артур Миранович <arturmiranovich@mail.ru>
2022-07-27 11:37:38 +0300

    begin

---
+++ RoomNumber/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomNumber
+{
+    public class Class1
+    {
+
+
+        public void Vvod()
+        {
+            var a = new List<int>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                a.Add(i);
+            }
+
+            TaTa(a);
+
+        }
+
+        public void TaTa(IEnumerable<int> a)
+        {
+
+            foreach (int resul in a)
+            {
+                Console.WriteLine(resul);
+            }    
+
+        }
+    }
+}
